487d1726819f4299ff6e5da99f2c7f88b09fa9ac
HazemIsmail-Prog <67516349+HazemIsmail-Prog@users.noreply.github.com>
2023-11-15 17:41:39 +0300

    remove bulk delete for drivers

--- app/Filament/Resources/DriverResource.php
+++ app/Filament/Resources/DriverResource.php
@@ -69,9 +69,9 @@ class DriverResource extends Resource
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+                // Tables\Actions\BulkActionGroup::make([
+                //     Tables\Actions\DeleteBulkAction::make(),
+                // ]),
             ])
             ->emptyStateActions([
                 Tables\Actions\CreateAction::make(),
