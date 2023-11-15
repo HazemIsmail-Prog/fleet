ef9a718716d727596db03f6fa4a0e47e753ec4fc
HazemIsmail-Prog <67516349+HazemIsmail-Prog@users.noreply.github.com>
2023-11-15 17:30:01 +0300

    add user type to form

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -42,6 +42,9 @@ class UserResource extends Resource
                 Forms\Components\TextInput::make('name')
                     ->required()
                     ->maxLength(255),
+                Forms\Components\TextInput::make('type')
+                    ->required()
+                    ->maxLength(255),
                 Forms\Components\TextInput::make('email')
                     ->email()
                     ->required()
